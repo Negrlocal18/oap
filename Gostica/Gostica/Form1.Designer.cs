30855ec35dc26dd88593975235b0c965ccfc2d43
Negrlocal18 <125107120+Negrlocal18@users.noreply.github.com>
2023-06-21 12:10:16 +0700

    Add files via upload

---
+++ Gostica/Gostica/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Gostica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnPocel = new System.Windows.Forms.Button();
+            this.btnNomera = new System.Windows.Forms.Button();
+            this.btnKlient = new System.Windows.Forms.Button();
+            this.BtnDashdoard = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelDesltopPane = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnCloseChildForm = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelDesltopPane.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panelMenu.Controls.Add(this.panel3);
+            this.panelMenu.Controls.Add(this.button7);
+            this.panelMenu.Controls.Add(this.button6);
+            this.panelMenu.Controls.Add(this.button5);
+            this.panelMenu.Controls.Add(this.button4);
+            this.panelMenu.Controls.Add(this.btnPocel);
+            this.panelMenu.Controls.Add(this.btnNomera);
+            this.panelMenu.Controls.Add(this.btnKlient);
+            this.panelMenu.Controls.Add(this.BtnDashdoard);
+            this.panelMenu.Controls.Add(this.panel2);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(186, 585);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.panel3.Location = new System.Drawing.Point(0, 144);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(3, 330);
+            this.panel3.TabIndex = 3;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button7.Location = new System.Drawing.Point(0, 538);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(186, 47);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button1";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button6.Location = new System.Drawing.Point(0, 426);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 47);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Таблица \"Cкидки при поселении\"";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button5.Location = new System.Drawing.Point(0, 379);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 47);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Таблица \"Комфортность\"";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button4.Location = new System.Drawing.Point(0, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 47);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Таблица \"Система скидок\"";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnPocel
+            // 
+            this.btnPocel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPocel.FlatAppearance.BorderSize = 0;
+            this.btnPocel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPocel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPocel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnPocel.Location = new System.Drawing.Point(0, 285);
+            this.btnPocel.Name = "btnPocel";
+            this.btnPocel.Size = new System.Drawing.Size(186, 47);
+            this.btnPocel.TabIndex = 4;
+            this.btnPocel.Text = "Таблица \"Поселение\"";
+            this.btnPocel.UseVisualStyleBackColor = true;
+            this.btnPocel.Click += new System.EventHandler(this.btnPocel_Click);
+            // 
+            // btnNomera
+            // 
+            this.btnNomera.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNomera.FlatAppearance.BorderSize = 0;
+            this.btnNomera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNomera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNomera.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnNomera.Location = new System.Drawing.Point(0, 238);
+            this.btnNomera.Name = "btnNomera";
+            this.btnNomera.Size = new System.Drawing.Size(186, 47);
+            this.btnNomera.TabIndex = 3;
+            this.btnNomera.Text = "Таблица \"Номера\"";
+            this.btnNomera.UseVisualStyleBackColor = true;
+            this.btnNomera.Click += new System.EventHandler(this.btnNomera_Click);
+            // 
+            // btnKlient
+            // 
+            this.btnKlient.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKlient.FlatAppearance.BorderSize = 0;
+            this.btnKlient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKlient.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKlient.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnKlient.Location = new System.Drawing.Point(0, 191);
+            this.btnKlient.Name = "btnKlient";
+            this.btnKlient.Size = new System.Drawing.Size(186, 47);
+            this.btnKlient.TabIndex = 2;
+            this.btnKlient.Text = "Таблица \"Клиенты\"";
+            this.btnKlient.UseVisualStyleBackColor = true;
+            this.btnKlient.Click += new System.EventHandler(this.btnKlient_Click);
+            // 
+            // BtnDashdoard
+            // 
+            this.BtnDashdoard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnDashdoard.FlatAppearance.BorderSize = 0;
+            this.BtnDashdoard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDashdoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDashdoard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.BtnDashdoard.Location = new System.Drawing.Point(0, 144);
+            this.BtnDashdoard.Name = "BtnDashdoard";
+            this.BtnDashdoard.Size = new System.Drawing.Size(186, 47);
+            this.BtnDashdoard.TabIndex = 1;
+            this.BtnDashdoard.Text = "Таблица \"Бронирование\"";
+            this.BtnDashdoard.UseVisualStyleBackColor = true;
+            this.BtnDashdoard.Click += new System.EventHandler(this.BtnDashdoard_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.label2.Location = new System.Drawing.Point(62, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label1.Location = new System.Drawing.Point(36, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Гостиница";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Gostica.Properties.Resources.branch;
+            this.pictureBox1.Location = new System.Drawing.Point(49, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelDesltopPane
+            // 
+            this.panelDesltopPane.Controls.Add(this.panel5);
+            this.panelDesltopPane.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesltopPane.Location = new System.Drawing.Point(186, 0);
+            this.panelDesltopPane.Name = "panelDesltopPane";
+            this.panelDesltopPane.Size = new System.Drawing.Size(833, 585);
+            this.panelDesltopPane.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel5.Controls.Add(this.btnCloseChildForm);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(833, 84);
+            this.panel5.TabIndex = 0;
+            // 
+            // btnCloseChildForm
+            // 
+            this.btnCloseChildForm.Location = new System.Drawing.Point(755, 21);
+            this.btnCloseChildForm.Name = "btnCloseChildForm";
+            this.btnCloseChildForm.Size = new System.Drawing.Size(51, 34);
+            this.btnCloseChildForm.TabIndex = 1;
+            this.btnCloseChildForm.Text = "Выйти";
+            this.btnCloseChildForm.UseVisualStyleBackColor = true;
+            this.btnCloseChildForm.Click += new System.EventHandler(this.btnCloseChildForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1019, 585);
+            this.Controls.Add(this.panelDesltopPane);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelDesltopPane.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnPocel;
+        private System.Windows.Forms.Button btnNomera;
+        private System.Windows.Forms.Button btnKlient;
+        private System.Windows.Forms.Button BtnDashdoard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelDesltopPane;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnCloseChildForm;
+    }
+}
+
---
+++ Gostica/Gostica/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gostica
+{
+    public partial class Form1 : Form
+    {
+        private Form activeForm;
+        private Button currentButton;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDashdoard_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form3(), sender);
+        }
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisaleButton();
+                    
+                    currentButton = (Button)btnSender;
+                    
+                    
+                    currentButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    
+                    
+                    btnCloseChildForm.Visible = true;
+                }
+            }
+        }
+        private void DisaleButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    
+                    previousBtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                }
+            }
+        }
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesltopPane.Controls.Add(childForm);
+            this.panelDesltopPane.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+
+
+        }
+
+        private void btnCloseChildForm_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form1(), sender);
+        }
+
+        private void btnKlient_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form4(), sender);
+        }
+
+        private void btnNomera_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form5(), sender);
+        }
+
+        private void btnPocel_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form6(), sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form7(), sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form8(), sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Form9(), sender);
+        }
+    }
+}
